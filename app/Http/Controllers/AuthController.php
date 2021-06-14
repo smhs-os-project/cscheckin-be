c9628141235ceccb79df7447a6aa9a6f607416dd
Sz-Wei Chen <swchen1217@gmail.com>
2021-06-14 13:57:06 +0800

    /auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\UserRepository;
+use Google_Client;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function createToken(Request $request, $org)
+    {
+        $id_token = $request->input('id_token');
+        $access_token = $request->input('access_token');
+
+        if (config('google.' . $org) == null) {
+            return response()->json(['error' => 'org_not_found'], Response::HTTP_NOT_FOUND);
+        }
+        $client = new Google_Client();  // Specify the CLIENT_ID of the app that accesses the backend
+        $client->setAuthConfig(config('google.' . $org . '.client_secret'));
+        $payload = $client->verifyIdToken($id_token);
+        if (!$payload) {
+            return response()->json(['error' => 'Invalid ID token'], Response::HTTP_UNAUTHORIZED);
+        }
+        $GID = $payload['sub'];
+        $domain = $payload['hd'] ?? 'gmail.com';
+        $user = $this->userRepository->findUserByGoogleUserId($GID);
+        if (!$user) {
+            $user = $this->userRepository->createUser([
+                'google_user_id' => $GID,
+                'domain' => $domain,
+                'name' => $payload['name'],
+                'email' => $payload['email'],
+                'photo' => $payload['picture']]);
+        }
+        $user_id = $user['id'];
+        $this->userRepository->setUserAccessToken($user_id, $access_token);
+        $token = $user->createToken('api-token');
+
+        return response()->json(['access_token' => $token->plainTextToken, 'token_type' => 'Bearer', 'exp' => $payload['exp']], Response::HTTP_CREATED);
+    }
+
+    public function whoami(Request $request)
+    {
+        $user = Auth::user();
+        $studentInfo = $this->userRepository->getStudentInfo($user['id']);
+        if ($studentInfo) {
+            $user['student'] = $studentInfo->only(['class', 'number']);
+        }
+
+        return response()->json($user, Response::HTTP_OK);
+    }
+
+    public function setStudent(Request $request)
+    {
+        $class = $request->input('class');
+        $number = $request->input('number');
+        $user = Auth::user();
+        $this->userRepository->setUserStudentInfo($user['id'], ['class' => $class, 'number' => $number]);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+
+    public function revokeToken(Request $request)
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Entities\StudentInformation;
+use App\Entities\User;
+use App\Entities\UserGoogleToken;
+
+class UserRepository
+{
+
+    /**
+     * @var User
+     */
+    private $user;
+    /**
+     * @var UserGoogleToken
+     */
+    private $userGoogleToken;
+    /**
+     * @var StudentInformation
+     */
+    private $studentInformation;
+
+    public function __construct(User $user, UserGoogleToken $userGoogleToken, StudentInformation $studentInformation)
+    {
+        $this->user = $user;
+        $this->userGoogleToken = $userGoogleToken;
+        $this->studentInformation = $studentInformation;
+    }
+
+    public function allUser()
+    {
+        return $this->user->all();
+    }
+
+    public function findUserById($id)
+    {
+        return $this->user->find($id);
+    }
+
+    public function findUserByGoogleUserId($googleUserId)
+    {
+        return $this->user->where('google_user_id', $googleUserId)->first();
+    }
+
+    public function findUserByDomain($domain)
+    {
+        return $this->user->where('domain', $domain)->get();
+    }
+
+    public function findUserByEmail($email)
+    {
+        return $this->user->where('email', $email)->first();
+    }
+
+    public function getUserAccessToken($userId)
+    {
+        return $this->userGoogleToken->where('user_id', $userId)->first();
+    }
+
+    public function getStudentInfo($userId)
+    {
+        return $this->studentInformation->where('user_id', $userId)->first();
+    }
+
+    public function createUser($data)
+    {
+        return $this->user->create($data);
+    }
+
+    public function setUserAccessToken($userId, $accessToken)
+    {
+        return $this->userGoogleToken->updateOrCreate(['user_id' => $userId], ['access_token' => $accessToken]);
+    }
+
+    public function setUserStudentInfo($userId, $studentInfo)
+    {
+        return $this->studentInformation->updateOrCreate(['user_id' => $userId], $studentInfo);
+    }
+
+    public function updateUser($id, $data)
+    {
+        return $this->user->where('id', $id)->update($data);
+    }
+
+    public function deleteUser($id)
+    {
+        return $this->user->find($id)->delete();
+    }
+}
