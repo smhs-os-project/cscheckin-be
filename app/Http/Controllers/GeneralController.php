27099df63f01ef7603777cfdbb3f9d0c4a11a15a
Sz-Wei Chen <swchen1217@gmail.com>
2021-06-15 01:35:59 +0800

    route /token

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class GeneralController extends Controller
 {
@@ -14,5 +15,14 @@ class GeneralController extends Controller
     public function isOk()
     {
         return response()->json(['success' => true]);
+    }
+
+    public function getInfo(Request $request, $org)
+    {
+        if (config('google.' . $org) == null) {
+            return response()->json(['error' => 'org_not_found'], Response::HTTP_NOT_FOUND);
+        }
+        $info = config('google.' . $org);
+        return response()->json(['client_id' => $info['client_id'], 'chinese_name' => $info['chinese_name']], Response::HTTP_OK);
     }
 }
--- config/google.php
+++ config/google.php
@@ -1,12 +1,18 @@
 <?php
 
 return [
-    'smhs'=>[
-        'client_id'=>storage_path('app/google_client/smhs_client_id.txt'),
-        'client_secret'=>storage_path('app/google_client/smhs_client_secret.json'),
+    'smhs' => [
+        'client_id' => storage_path('app/google_client/smhs_client_id.txt'),
+        'client_secret' => storage_path('app/google_client/smhs_client_secret.json'),
+        'chinese_name' => '高雄市立三民高中',
     ],
-    'fssh'=>[
-        'client_id'=>storage_path('app/google_client/fssh_client_id.txt'),
-        'client_secret'=>storage_path('app/google_client/fssh_client_secret.json'),
+    'fssh' => [
+        'client_id' => storage_path('app/google_client/fssh_client_id.txt'),
+        'client_secret' => storage_path('app/google_client/fssh_client_secret.json'),
+        'chinese_name' => '國立鳳山高中',
     ],
+    'MAPPING' => [
+        'smhs.kh.edu.tw' => 'smhs',
+        'fssh.khc.edu.tw' => 'fssh',
+    ]
 ];
