e3aa13138eb0d0a3466d65ba020948ed86459120
Sz-Wei Chen <swchen1217@gmail.com>
2021-06-15 08:31:32 +0800

    route /info

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -17,12 +17,28 @@ class GeneralController extends Controller
         return response()->json(['success' => true]);
     }
 
-    public function getInfo(Request $request, $org)
+    public function getInfo(Request $request, $org = null)
     {
-        if (config('google.' . $org) == null) {
-            return response()->json(['error' => 'org_not_found'], Response::HTTP_NOT_FOUND);
+        if ($org) {
+            if (config('google.' . $org) == null) {
+                return response()->json(['error' => 'org_not_found'], Response::HTTP_NOT_FOUND);
+            }
+            $info = config('google.' . $org);
+            $file = fopen($info['client_id'], "r");
+            $client_id = fgets($file, filesize($info['client_id']));
+            return response()->json(['client_id' => $client_id, 'chinese_name' => $info['chinese_name']], Response::HTTP_OK);
+        } else {
+            $info = config('google');
+            $result = array();
+            foreach ($info as $key => $value) {
+                if ($key == 'MAPPING')
+                    continue;
+                $file = fopen($value['client_id'], "r");
+                $client_id = fgets($file, filesize($value['client_id']));
+                $result[] = ['client_id' => $client_id, 'chinese_name' => $value['chinese_name']];
+            }
+            return response()->json($result, Response::HTTP_OK);
         }
-        $info = config('google.' . $org);
-        return response()->json(['client_id' => $info['client_id'], 'chinese_name' => $info['chinese_name']], Response::HTTP_OK);
+
     }
 }
--- config/google.php
+++ config/google.php
@@ -2,12 +2,12 @@
 
 return [
     'smhs' => [
-        'client_id' => storage_path('app/google_client/smhs_client_id.txt'),
+        'client_id' => storage_path('app/google_client/smhs_client_id.key'),
         'client_secret' => storage_path('app/google_client/smhs_client_secret.json'),
         'chinese_name' => '高雄市立三民高中',
     ],
     'fssh' => [
-        'client_id' => storage_path('app/google_client/fssh_client_id.txt'),
+        'client_id' => storage_path('app/google_client/fssh_client_id.key'),
         'client_secret' => storage_path('app/google_client/fssh_client_secret.json'),
         'chinese_name' => '國立鳳山高中',
     ],
