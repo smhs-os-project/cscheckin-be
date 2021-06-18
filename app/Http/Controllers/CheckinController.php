7545e1b9d1b6fee5f240fd4e9aeb644b9f87e799
Sz-Wei Chen <swchen1217@gmail.com>
2021-06-18 17:31:32 +0800

    Enable getCourseCheckin cache

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -44,56 +44,54 @@ class CheckinController extends Controller
         if ($course['teacher_id'] != $user['id']) {
             return response()->json(['error' => 'you_cannot_access_course'], Response::HTTP_FORBIDDEN);
         }
-        /*$result = Cache::tags('checkin')->remember($courseId, Carbon::now()->addMinutes(60), function () use ($courseId, $course) {
-
-            return array_merge($checkedIn, $notCheckedIn);
-        });*/
-        $allStu = $this->courseRepository->getStudentByGoogleClassroomId($course['google_classroom_id'])->sortBy('name');
-        $checkinStu = $this->checkinRepository->findCheckinByCourseId($courseId);
-        $checkedIn = array();
-        $notCheckedIn = array();
-        foreach ($allStu as $item) {
-            $uu = $this->userRepository->findUserByGoogleUserId($item['google_user_id']);
-            if ($uu) {
-                $si = $this->userRepository->getStudentInfo($uu['id']);
-                $checkin = $checkinStu->where('student_id', $uu['id'])->first();
-                if ($checkin) {
-                    $checkedIn[] = [
-                        'checkin_id' => $checkin['id'],
-                        'state' => $checkin['state'],
-                        'created_at' => $checkin['created_at'],
-                        'name' => $item['name'],
-                        'class' => strval($si['class']) ?? '',
-                        'number' => strval($si['number']) ?? '',
-                    ];
+        $result = Cache::tags('checkin')->remember($courseId, Carbon::now()->addMinutes(60), function () use ($courseId, $course) {
+            $allStu = $this->courseRepository->getStudentByGoogleClassroomId($course['google_classroom_id'])->sortBy('name');
+            $checkinStu = $this->checkinRepository->findCheckinByCourseId($courseId);
+            $checkedIn = array();
+            $notCheckedIn = array();
+            foreach ($allStu as $item) {
+                $uu = $this->userRepository->findUserByGoogleUserId($item['google_user_id']);
+                if ($uu) {
+                    $si = $this->userRepository->getStudentInfo($uu['id']);
+                    $checkin = $checkinStu->where('student_id', $uu['id'])->first();
+                    if ($checkin) {
+                        $checkedIn[] = [
+                            'checkin_id' => $checkin['id'],
+                            'state' => $checkin['state'],
+                            'created_at' => $checkin['created_at'],
+                            'name' => $item['name'],
+                            'class' => strval($si['class']) ?? '',
+                            'number' => strval($si['number']) ?? '',
+                        ];
+                    } else {
+                        $notCheckedIn[] = [
+                            'checkin_id' => -1,
+                            'state' => 'NOT_CHECKED_IN',
+                            'created_at' => '',
+                            'name' => $item['name'],
+                            'class' => strval($si['class']) ?? '',
+                            'number' => strval($si['number']) ?? '',
+                        ];
+                    }
                 } else {
                     $notCheckedIn[] = [
                         'checkin_id' => -1,
                         'state' => 'NOT_CHECKED_IN',
                         'created_at' => '',
                         'name' => $item['name'],
-                        'class' => strval($si['class']) ?? '',
-                        'number' => strval($si['number']) ?? '',
+                        'class' => '',
+                        'number' => '',
                     ];
                 }
-            } else {
-                $notCheckedIn[] = [
-                    'checkin_id' => -1,
-                    'state' => 'NOT_CHECKED_IN',
-                    'created_at' => '',
-                    'name' => $item['name'],
-                    'class' => '',
-                    'number' => '',
-                ];
             }
-        }
-        $c = array_column($checkedIn, 'class');
-        $n = array_column($checkedIn, 'number');
-        array_multisort($c, SORT_ASC, $n, SORT_ASC, $checkedIn);
-        $c = array_column($notCheckedIn, 'class');
-        $n = array_column($notCheckedIn, 'number');
-        array_multisort($c, SORT_ASC, $n, SORT_ASC, $notCheckedIn);
-        $result = array_merge($notCheckedIn, $checkedIn);
+            $c = array_column($checkedIn, 'class');
+            $n = array_column($checkedIn, 'number');
+            array_multisort($c, SORT_ASC, $n, SORT_ASC, $checkedIn);
+            $c = array_column($notCheckedIn, 'class');
+            $n = array_column($notCheckedIn, 'number');
+            array_multisort($c, SORT_ASC, $n, SORT_ASC, $notCheckedIn);
+            return array_merge($notCheckedIn, $checkedIn);
+        });
 
         return response()->json($result, Response::HTTP_OK);
     }
